e7a7158bb511bad29317c650361f51cb5a2027a3
Aaron Christiansen <aaronc20000@gmail.com>
2021-03-25 20:33:49 +0000

    Add proper function calling

--- src/script_engine.rs
+++ src/script_engine.rs
@@ -22,6 +22,8 @@ pub enum Instruction {
     Push(Object),
     Pop,
     Return,
+    Call,
+    Halt,
 }
 
 #[derive(PartialEq, Eq, Debug, Clone)]
@@ -46,32 +48,64 @@ impl InterpreterFrame {
 }
 
 #[derive(PartialEq, Eq, Debug, Clone)]
-pub enum ExecutionResult {
+pub enum InstructionExecutionResult {
     Ok,
     OkReturn,
+    OkHalt,
     OkNewFrame(InterpreterFrame),
     Err(String),
 }
 
+#[derive(PartialEq, Eq, Debug, Clone)]
+pub enum FrameExecutionResult {
+    Ok,
+    OkHalt,
+    Err(String)
+}
+
 impl InterpreterFrame {
-    pub fn execute_one_instruction(&mut self) -> ExecutionResult {
+    pub fn execute_one_instruction(&mut self) -> InstructionExecutionResult {
         let instruction = &(*self.function).body[self.ip];
         let mut will_increment_ip = true;
         
         let result = match instruction {
             Instruction::Push(obj) => {
                 self.stack.push(obj.clone());
-                ExecutionResult::Ok
-            },
+                InstructionExecutionResult::Ok
+            }
 
             Instruction::Pop => {
                 self.stack.pop();
-                ExecutionResult::Ok
+                InstructionExecutionResult::Ok
             }
 
             Instruction::Return => {
-                ExecutionResult::OkReturn
+                InstructionExecutionResult::OkReturn
             }
+
+            Instruction::Call => {
+                let object = self.stack.pop().expect("stack empty");
+                if let Object::Function(func_arc) = object {
+                    let function = func_arc.borrow();
+                    let Function { parameters, .. } = function;
+
+                    // Collect arguments
+                    let mut arguments = vec![];
+                    for _ in 0..parameters.len() {
+                        arguments.push(self.stack.pop().expect("not enough stack items for arguments"));
+                    }
+
+                    // Create new frame
+                    InstructionExecutionResult::OkNewFrame(InterpreterFrame {
+                        arguments,
+                        ..InterpreterFrame::new(func_arc)
+                    })
+                } else {
+                    panic!("calling a non-function");
+                }
+            }
+
+            Instruction::Halt => InstructionExecutionResult::OkHalt
         };
 
         if will_increment_ip {
@@ -92,28 +126,44 @@ impl InterpreterState {
         self.frames.last_mut().expect("no frames")
     }
 
-    pub fn execute_one_instruction(&mut self) {
+    pub fn execute_one_instruction(&mut self) -> FrameExecutionResult {
         let result = self.current_frame().execute_one_instruction();
         match result {
-            ExecutionResult::Ok => (),
-            ExecutionResult::OkReturn => {
-                if self.current_frame().stack.len() != 1 {
-                    panic!("returning frame should have exactly one stack item");
+            InstructionExecutionResult::Ok => FrameExecutionResult::Ok,
+            InstructionExecutionResult::OkHalt => FrameExecutionResult::OkHalt,
+            InstructionExecutionResult::OkReturn => {
+                if self.current_frame().stack.is_empty() {
+                    return FrameExecutionResult::Err(
+                        "returning frame has empty stack".into()
+                    );
                 }
 
                 // Get return value
-                let return_value = self.current_frame().stack[0].clone();
+                let return_value = self.current_frame().stack.last().unwrap().clone();
                 
                 // Discard top stack frame
                 self.frames.pop();
 
                 // Push return value onto the new top stack frame's stack
                 self.current_frame().stack.push(return_value);
+
+                FrameExecutionResult::Ok
             },
-            ExecutionResult::OkNewFrame(frame) => {
+            InstructionExecutionResult::OkNewFrame(frame) => {
                 self.frames.push(frame);
+                FrameExecutionResult::Ok
             }
-            ExecutionResult::Err(s) => panic!(s),
+            InstructionExecutionResult::Err(s) => FrameExecutionResult::Err(s),
         }
-    }    
+    }
+
+    pub fn execute_until_halt(&mut self) {
+        loop {
+            match self.execute_one_instruction() {
+                FrameExecutionResult::Ok => (),
+                FrameExecutionResult::OkHalt => break,
+                FrameExecutionResult::Err(s) => panic!(s),
+            }
+        } 
+    }
 }
--- src/tests/script_engine_tests.rs
+++ src/tests/script_engine_tests.rs
@@ -4,7 +4,7 @@ use std::sync::Arc;
 
 #[test]
 fn it_can_work_with_the_stack() {
-    let function = Function {
+    let function = Arc::new(Function {
         parameters: vec![],
         body: vec![
             Instruction::Push(Object::Integer(3)),
@@ -14,25 +14,59 @@ fn it_can_work_with_the_stack() {
             Instruction::Pop,
             Instruction::Pop,
         ]
-    };
+    });
 
-    let mut frame = InterpreterFrame::new(Arc::new(function));
+    let mut frame = InterpreterFrame::new(function);
 
-    assert_eq!(frame.execute_one_instruction(), ExecutionResult::Ok);
+    assert_eq!(frame.execute_one_instruction(), InstructionExecutionResult::Ok);
     assert_eq!(frame.stack, vec![Object::Integer(3)]);
 
-    assert_eq!(frame.execute_one_instruction(), ExecutionResult::Ok);
+    assert_eq!(frame.execute_one_instruction(), InstructionExecutionResult::Ok);
     assert_eq!(frame.stack, vec![Object::Integer(3), Object::Boolean(true)]);
 
-    assert_eq!(frame.execute_one_instruction(), ExecutionResult::Ok);
+    assert_eq!(frame.execute_one_instruction(), InstructionExecutionResult::Ok);
     assert_eq!(frame.stack, vec![Object::Integer(3)]);
 
-    assert_eq!(frame.execute_one_instruction(), ExecutionResult::Ok);
+    assert_eq!(frame.execute_one_instruction(), InstructionExecutionResult::Ok);
     assert_eq!(frame.stack, vec![Object::Integer(3), Object::Null]);
 
-    assert_eq!(frame.execute_one_instruction(), ExecutionResult::Ok);
+    assert_eq!(frame.execute_one_instruction(), InstructionExecutionResult::Ok);
     assert_eq!(frame.stack, vec![Object::Integer(3)]);
 
-    assert_eq!(frame.execute_one_instruction(), ExecutionResult::Ok);
+    assert_eq!(frame.execute_one_instruction(), InstructionExecutionResult::Ok);
     assert_eq!(frame.stack, vec![]);
+}
+
+#[test]
+fn it_can_return() {
+    let called_function = Arc::new(Function {
+        parameters: vec![],
+        body: vec![
+            // Some other garbage on the stack
+            Instruction::Push(Object::Integer(1)),
+            Instruction::Push(Object::Integer(2)),
+
+            // What we actually return
+            Instruction::Push(Object::Integer(3)),
+            Instruction::Return,
+        ]
+    });
+
+    let function = Arc::new(Function {
+        parameters: vec![],
+        body: vec![
+            Instruction::Push(Object::Integer(4)),
+            Instruction::Push(Object::Function(called_function)),
+            Instruction::Call,
+            Instruction::Halt,
+        ]
+    });
+
+    let mut state = InterpreterState::default();
+    state.frames.push(InterpreterFrame::new(function));
+
+    state.execute_until_halt();
+
+    assert_eq!(state.frames.len(), 1);
+    assert_eq!(state.frames[0].stack, vec![Object::Integer(4), Object::Integer(3)]);
 }
