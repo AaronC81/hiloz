469899be7c46d5a66cf92c3e55eb1fc21c404f78
Aaron Christiansen <aaronc20000@gmail.com>
2021-03-27 16:10:01 +0000

    Add basic script block parsing

--- src/script_parser.rs
+++ src/script_parser.rs
@@ -12,7 +12,7 @@ use std::error::Error;
 pub enum Node {
     Constant(se::Object),
     Identifier(String),
-    Assignment { target: Box<Node>, value: Box<Node> },
+    PinAssignment { target: Box<Node>, value: Box<Node> },
     Accessor { target: Box<Node>, name: Box<Node> },
     Sleep(Box<Node>),
     Return(Box<Node>),
@@ -22,6 +22,7 @@ pub enum Node {
     ComponentDefinition { name: String, body: Box<Node> },
     ConstructorDefinition { parameters: Vec<String>, body: Box<Node> },
     FunctionDefinition { name: String, parameters: Vec<String>, body: Box<Node> },
+    ScriptDefinition(Box<Node>),
 
     ComponentInstantiation { instance_name: Box<Node>, component_name: Box<Node>, arguments: Vec<Node> },
     Connect(Vec<Node>),
@@ -38,6 +39,8 @@ fn must_space() -> Parser<u8, ()> { is_a(multispace).repeat(1..).discard().name(
 fn semi() -> Parser<u8, ()> { (space() + sym(b';') + space()).discard().name("semicolon") }
 fn lbrace() -> Parser<u8, ()> { (space() + sym(b'{') + space()).discard().name("left brace") }
 fn rbrace() -> Parser<u8, ()> { (space() + sym(b'}') + space()).discard().name("right brace") }
+fn lparen() -> Parser<u8, ()> { (space() + sym(b'(') + space()).discard().name("left parenthesis") }
+fn rparen() -> Parser<u8, ()> { (space() + sym(b')') + space()).discard().name("right parenthesis") }
 
 pub fn raw_integer() -> Parser<u8, i64> {
     (sym(b'-').opt() + is_a(digit).repeat(1..))
@@ -51,7 +54,7 @@ pub fn integer() -> Parser<u8, Node> {
 }
 
 pub fn raw_identifier() -> Parser<u8, String> {
-    // TODO: reject keywords
+    // TODO: reject keywords or logic value literals
     ((is_a(alpha) | sym(b'_')) + (is_a(alpha) | is_a(digit) | sym(b'_')).repeat(0..))
         .collect()
         .convert(from_utf8)
@@ -62,9 +65,20 @@ pub fn identifier() -> Parser<u8, Node> {
     raw_identifier().map(|s| Identifier(s.into()))
 }
 
+pub fn logic_value() -> Parser<u8, Node> {
+    sym(b'H').map(|_| Constant(LogicValue(logic::Value::High)))
+    | sym(b'L').map(|_| Constant(LogicValue(logic::Value::Low)))
+    | sym(b'X').map(|_| Constant(LogicValue(logic::Value::Unknown)))
+}
+
 pub fn pin_definition() -> Parser<u8, Node> {
     (seq(b"pin") + must_space() + raw_identifier() + semi())
         .map(|((_, id), _)| PinDefinition(id))
+}
+
+pub fn script_definition() -> Parser<u8, Node> {
+    (seq(b"script") + space() + script_block())
+        .map(|(_, body)| ScriptDefinition(Box::new(body)))
 }
 
 pub fn component_definition() -> Parser<u8, Node> {
@@ -82,8 +96,41 @@ pub fn component_definition() -> Parser<u8, Node> {
 pub fn component_definition_body() -> Parser<u8, Node> {
     (
         lbrace()
-        + (pin_definition()).repeat(0..)
+        + (pin_definition() | script_definition()).repeat(0..)
         + rbrace()
     )
         .map(|((_, defs), _)| Node::Body(defs))
+}
+
+pub fn script_block() -> Parser<u8, Node> {
+    (
+        lbrace()
+        + space()
+        + script_statement().repeat(0..)
+        + space()
+        + rbrace()
+    )
+        .map(|(((_, e), _), _)| Body(e))
+}
+
+pub fn script_statement() -> Parser<u8, Node> {
+    ((script_sleep_statement() | pin_assignment() | script_expression())
+        + space() + semi()).map(|((e, _), _)| e)
+}
+
+pub fn script_expression() -> Parser<u8, Node> {
+    integer() | logic_value() | identifier()
+}
+
+pub fn pin_assignment() -> Parser<u8, Node> {
+    (identifier() + space() + seq(b"<-") + space() + script_expression())
+        .map(|((((i, _), _), _), e)| PinAssignment {
+            target: Box::new(i),
+            value: Box::new(e),
+        })
+}
+
+pub fn script_sleep_statement() -> Parser<u8, Node> {
+    (seq(b"sleep") + space() + lparen() + space() + script_expression() + space() + rparen())
+        .map(|(((_, e), _), _)| Sleep(Box::new(e)))
 }
--- src/tests/script_parser_tests.rs
+++ src/tests/script_parser_tests.rs
@@ -1,6 +1,7 @@
 use crate::script_parser::*;
 use crate::script_engine::Object::*;
 use crate::script_parser::Node::*;
+use crate::logic::*;
 
 #[test]
 fn it_parses_integers() {
@@ -19,10 +20,31 @@ fn it_parses_component_definitions() {
     assert_eq!(component_definition().parse(b"define component Something {
         pin a;
         pin b;
+
+        script {
+            a <- H;
+            sleep(1000);
+            a <- L;
+            sleep(1000);
+        }
     }").unwrap(), ComponentDefinition {
         name: "Something".into(),
-        body: Box::new(
-            Body(vec![PinDefinition("a".into()), PinDefinition("b".into())])
-        )
+        body: Box::new(Body(vec![
+            PinDefinition("a".into()),
+            PinDefinition("b".into()),
+            ScriptDefinition(Box::new(Body(vec![
+                PinAssignment {
+                    target: Box::new(Identifier("a".into())),
+                    value: Box::new(Constant(LogicValue(Value::High)))
+                },
+                Sleep(Box::new(Constant(Integer(1000)))),
+                PinAssignment {
+                    target: Box::new(Identifier("a".into())),
+                    value: Box::new(Constant(LogicValue(Value::Low)))
+                },
+                Sleep(Box::new(Constant(Integer(1000)))),
+            ])))
+        ])),
+
     })
 }
